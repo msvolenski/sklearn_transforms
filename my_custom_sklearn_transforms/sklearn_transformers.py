ab55702eece5cd7baf59fd1dc91906e70912ae18
Matheus Svolenski <msvolenski@gmail.com>
2020-08-22 16:12:59 -0300

    New DropRows transform

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,21 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+# All sklearn Transforms must have the `transform` and `fit` methods
+class DropRows(BaseEstimator, TransformerMixin):
+    def __init__(self, condition_to_drop):
+        self.condition = condition_to_drop
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        
+        # Separamos as linhas indesejadas
+        rm_index = data[self.condition].index.values
+        
+        # Retornamos um novo dataframe sem as linhas indesejadas        
+        return data.drop(labels=rm_index, axis='index')
